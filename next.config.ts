52fb0782a7f9ec879d7750c15baebe599b52551f
sengardeep <sengardeep2006@gmail.com>
2026-04-17 11:08:27 +0530

    Init

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactCompiler : true,
   experimental : {
     turbopackFileSystemCacheForDev : true
-  }
+  },
+  cacheComponents : true
 };
 
 export default nextConfig;
